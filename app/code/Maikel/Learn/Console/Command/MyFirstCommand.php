670f8781f04049b5e3d1b212c8ac9595ade99c60
Maikel Vitancourt <maikel-93@hotmail.com>
2020-12-01 14:23:54 -0300

    Created my:first:command console command at Maikel_Learn module

---
+++ app/code/Maikel/Learn/Console/Command/MyFirstCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Maikel\Learn\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class MyFirstCommand
+ */
+class MyFirstCommand extends Command
+{
+    const NAME = 'name';
+
+    /**
+     * @inheritDoc
+     */
+    protected function configure()
+    {
+        $this->setName('my:first:command');
+        $this->setDescription('This is my first console command.');
+        $this->addOption(
+            self::NAME,
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Name'
+        );
+
+        parent::configure();
+
+        return $this;
+    }
+
+    /**
+     * Execute the command
+     *
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return null|int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($name = $input->getOption(self::NAME)) {
+            $output->writeln('<info>Provided name is `' . $name . '`</info>');
+        }
+
+        $output->writeln('<info>Success Message.</info>');
+        $output->writeln('<error>An error encountered.</error>');
+        $output->writeln('<comment>Some Comment.</comment>');
+
+        return $this;
+    }
+}
