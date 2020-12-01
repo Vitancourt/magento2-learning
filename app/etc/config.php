acec925a9c6da394f7cb6174457792b5bd229289
Maikel Vitancourt <maikel-93@hotmail.com>
2020-12-01 13:58:50 -0300

    Created a module named Maikel_Learn

--- app/etc/config.php
+++ app/etc/config.php
@@ -327,6 +327,7 @@ return [
         'Klarna_Kp' => 1,
         'Klarna_Onsitemessaging' => 1,
         'Klarna_KpGraphQl' => 1,
+        'Maikel_Learn' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
         'Temando_ShippingRemover' => 1,
